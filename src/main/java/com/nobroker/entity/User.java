017f996c6317cc3fd152423072ef9177a206e410
satyam <satyamsinhaha@gmail.com>
2024-01-14 14:58:18 +0530

    5th Jan 2024

---
+++ src/main/java/com/nobroker/entity/User.java
@@ -0,0 +1,27 @@
+package com.nobroker.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    private String email;
+
+    private String mobile;
+
+    private String password;
+}
--- src/main/java/com/nobroker/service/impl/OwnerPlanServiceImpl.java
+++ src/main/java/com/nobroker/service/impl/OwnerPlanServiceImpl.java
@@ -7,6 +7,9 @@ import com.nobroker.payload.OwnerPlanDto;
 import com.nobroker.repository.OwnerPlanRepository;
 import com.nobroker.service.OwnerPlanService;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class OwnerPlanServiceImpl implements OwnerPlanService {
 
@@ -25,6 +28,13 @@ public class OwnerPlanServiceImpl implements OwnerPlanService {
         return mapToDto(save);
     }
 
+    @Override
+    public List<OwnerPlanDto> getAllOwnerPlans() {
+        List<OwnerPlan> ownerPlans = ownerPlanRepository.findAll();
+        List<OwnerPlanDto> ownerPlanDto = ownerPlans.stream().map(plan -> mapToDto(plan)).collect(Collectors.toList());
+         return ownerPlanDto;
+    }
+
     OwnerPlan mapToEntity(OwnerPlanDto ownerPlanDto){
         OwnerPlan ownerplan = modelMapper.map(ownerPlanDto, OwnerPlan.class);
         return ownerplan;
@@ -32,6 +42,6 @@ public class OwnerPlanServiceImpl implements OwnerPlanService {
     OwnerPlanDto mapToDto(OwnerPlan ownerPlan){
         OwnerPlanDto ownerPlanDto = modelMapper.map(ownerPlan, OwnerPlanDto.class);
         return ownerPlanDto;
-}
+    }
 
 }
